ef12fe359ee87bf3850dc2827b1eb518d8859e02
Musembi_ <146333442+musembi22@users.noreply.github.com>
2024-02-28 00:13:56 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+username = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enter your location:")
+print("My name is"+ " "+ username +" "+"iam" + " "+ age + " years of age" +" "+" and i come from"+" " + location)
